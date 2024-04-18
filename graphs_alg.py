af601239e40f76662551c603d339026b06f61f66
gabriel-2802 <gabrielvalentine738@gmail.com>
2024-04-18 19:51:29 +0300

    tarjan's hell

--- graphs_alg.py
+++ graphs_alg.py
@@ -3,12 +3,12 @@ from typing import List
 # tested
 def complement_graph(graph: List[List[int]]) -> List[List[int]]:
     # create the empty complement graph
-    n = len(graph) + 1
+    n = len(graph)
     complement = [[] for _ in range(n + 1)]
 
 
     for node in range(1, n):
-        for neighbor in graph[node - 1]:
+        for neighbor in graph[node]:
             complement[neighbor].append(node)
     return complement
 
@@ -16,26 +16,26 @@ def complement_graph(graph: List[List[int]]) -> List[List[int]]:
 # tested
 def scc_kosaraju(graph: List[List[int]]) -> List[List[int]]:
     n = len(graph)
-    visited = [False] * (n + 1)  # Visited list with extra space for 1-based indexing
+    visited = [False] * (n)  # Visited list with extra space for 1-based indexing
 
     stack = []
     scc = []
 
     def dfs(node):
         visited[node] = True
-        for neighbor in graph[node - 1]:  # Translate to 0-index to access neighbors
+        for neighbor in graph[node]:  # Translate to 0-index to access neighbors
             if not visited[neighbor]:
                 dfs(neighbor)
 
         stack.append(node) 
 
-    for i in range(1, n + 1):
+    for i in range(1, n):
         if not visited[i]:
             dfs(i)
 
     # Get the transpose of the graph
     transposed_graph = complement_graph(graph)
-    visited = [False] * (n + 1)  # Reset visited for the second pass
+    visited = [False] * (n)  # Reset visited for the second pass
 
     def dfs2(node, cur_scc):
         visited[node] = True
--- tarjan_alg.py
+++ tarjan_alg.py
@@ -1,16 +1,19 @@
 from typing import List
+from typing import Set
+from typing import Tuple
 from graphs_alg import scc_kosaraju
 
 
 # tested
+# used for directed graphs
 def scc_tarjan(graph: List[List[int]]) -> List[List[int]]:
-    n = len(graph) + 1
+    n = len(graph)
 
     # Initialize variables
     timestamp = 0
-    visited = [False] * n  # Visited nodes (not using index 0)
-    low = [0] * n  # Low link value of each node (not using index 0)
-    discover_t = [-1] * n  # Discovery time of each node (not using index 0)
+    visited = [False] * (n)  # Visited nodes (not using index 0)
+    low = [0] * (n)  # Low link value of each node (not using index 0)
+    discover_t = [-1] * (n)  # Discovery time of each node (not using index 0)
 
     # stack and set to keep track of nodes in the current 
     stackNodes = []
@@ -27,7 +30,7 @@ def scc_tarjan(graph: List[List[int]]) -> List[List[int]]:
 
         visited[node] = True
 
-        for neighbor in graph[node - 1]:
+        for neighbor in graph[node]:
             if not visited[neighbor]:
                 dfs(neighbor)
                 low[node] = min(low[node], low[neighbor])
@@ -51,16 +54,93 @@ def scc_tarjan(graph: List[List[int]]) -> List[List[int]]:
 
     return all_scc
 
+def critical_vertexes_tarjan(graph: List[List[int]]) -> Set[int]:
+    n = len(graph)
+
+    # Initialize variables
+    timestamp = 0
+    visited = [False] * (n)
+    discover_t = [-1] * (n)
+    low = [0] * (n)
+    parent = [-1] * (n) # addition to Tarjan's algorithm
+    critical_vertexes = set()
+
+    def dfs(node):
+        children = 0 # children of the current node
+        visited[node] = True
+
+        nonlocal timestamp
+        timestamp += 1
+        discover_t[node] = low[node] = timestamp
+
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                children += 1
+                parent[neighbor] = node
+                dfs(neighbor)
+
+                # check if the subtree rooted at neighbor has a back edge to an ancestor of node
+                low[neighbor] = min(low[neighbor], low[node])
+
+                # node is CV if it s root and has more than one child
+                if parent[node] == -1 and children > 1:
+                    critical_vertexes.add(node)
+
+                # node is CV if it is not root and has a child that has a back edge to an ancestor of node
+                if parent[node] != -1 and low[neighbor] >= discover_t[node]:
+                    critical_vertexes.add(node)
+
+            elif neighbor != parent[node]:
+                low[node] = min(low[node], discover_t[neighbor])
 
 
+    for i in range(1, n):
+        if not visited[i]:
+            dfs(i)
+
+    return critical_vertexes
+    
+def critical_edges_tarjan(graph: List[List[int]]) -> Set[Tuple[int, int]]:
+    n = len(graph)
+
+    # Initialize variables
+    visited = [False] * (n)
+    discover_t = [-1] * (n)
+    parent = [-1] * (n)
+    low = [0] * (n)
+    timestamp = 0
+
+    critical_edges = set()
+
+    def dfs(node):
+        nonlocal timestamp
+        timestamp += 1
+        visited[node] = True
+        discover_t[node] = low[node] = timestamp
 
 
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                parent[neighbor] = node
+                dfs(neighbor)
+                low[node] = min(low[node], low[neighbor])
 
+                if low[neighbor] > discover_t[node]:
+                    critical_edges.add((node, neighbor))
+            elif neighbor != parent[node]:
+                low[node] = min(low[node], discover_t[neighbor])
 
-
+    
+    for i in range(1, n):
+        if not visited[i]:
+            dfs(i)
+    
+    return critical_edges
+    
 
 # one component
 graph = [
+    [],
    [2],
    [3],
    [1]
@@ -68,6 +148,7 @@ graph = [
 
 # one component
 graph2 = [
+    [],
     [2],
     [3],
     [1, 4],
@@ -78,6 +159,7 @@ graph2 = [
 
 # 3 components
 graph3 = [
+    [],
     [2],
     [1, 3],
     [4],
@@ -85,14 +167,14 @@ graph3 = [
     [4,5]
 ]
 
-print(scc_tarjan(graph))
-#print(scc_kosaraju(graph))
+# print(scc_tarjan(graph))
+# print(scc_kosaraju(graph))
 
 
-print(scc_tarjan(graph2))
-#print(scc_kosaraju(graph2))
+# print(scc_tarjan(graph2))
+# print(scc_kosaraju(graph2))
 
-print(scc_tarjan(graph3))
-#print(scc_kosaraju(graph3))
+# print(scc_tarjan(graph3))
+# print(scc_kosaraju(graph3))
 
     
