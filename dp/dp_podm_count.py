1260dccfbc48d341a05b5013ba243176104a34b3
mariosam23 <sampetrumario11@gmail.com>
2024-04-19 13:36:38 +0300

    Done dp

--- dp/dp_podm_count.py
+++ dp/dp_podm_count.py
@@ -63,14 +63,95 @@ def matrix_mult_order(arr: List[int]) -> int:
 	return dp[1][n]
 
 
+"""
+Counting category
+-----------------
+Characteristics:
+	--> Recurrences have a common similarity: they all count things.
+	--> Cases where the number sought is very large are interesting. In this case,
+	  		the remainder of the division of the number sought by a number MOD is required.
+	--> When looking for a recurrence for a counting problem, 
+			you need to be careful about two aspects:
+				1. Do not count the same object twice.
+				2. Count all objects in question.
+	--> A counting problem often involves partitioning all possible solutions according
+	  		to a certain criterion. Finding the criterion is the essential part for finding the recurrence.
+	--> There are rules for working with residue classes and mathematical properties that you
+	 		should keep in mind when implementing to correctly obtain the remainders of certain expressions.
+"""
 
 
+"""
+--- A piece dimension is 4 x 1. It can be placed horizontally or vertically.
+	Every fence built has to be of height 4.
+--- How many fences of length n and height 4 exist?
+
+--- Ex: n <= 3 -> 0, n == 4 -> 2.
+
+--- Tested and 100% correct
+"""
+def gigel_fence(n: int) -> int:
+	MOD = 1009
+
+	if n <= 3:
+		return 1
+	if n == 4:
+		return 2
+
+	dp = [0] * (n + 1)
+	# dp[i] -> number of fences of length i and height 4
+	dp[1] = dp[2] = dp[3] = 1
+	dp[4] = 2
+
+	# Iterate over the lengths of fences
+	for i in range(5, n + 1):
+		# Calculate the number of fences of length i and height 4
+		dp[i] = (dp[i - 1] + dp[i - 4]) % MOD
+
+	return dp[n]
 
 
-def main():
-	arr = [13, 5, 89, 3, 34]
-	print(matrix_mult_order(arr))
+"""
+--- Tested and 100% correct
+"""
+def number_of_subsequences_of_even_sum(arr: List[int]) -> int:
+	MOD = 1000000007
+	n = len(arr)
 
+	# Number of subsequences with even sum using the first i elements and ending with the i-th element
+	dp_even = [0] * (n + 1)
+	# Same as above, but for odd sum
+	dp_odd = [0] * (n + 1)
 
-if __name__ == "__main__":
-	main()
+	# Partial sums
+	sum_even = 0 # dp_even[1] + dp_even[2] + ... + dp_even[i]
+	sum_odd = 0 # dp_odd[1] + dp_odd[2] + ... + dp_odd[i]
+
+	# Iterate over the elements of the array
+	for i in range(1, n + 1):
+		# If the i-th element is even
+		if arr[i - 1] % 2 == 0:
+			# arr[i] alone
+			dp_even[i] = 1
+			# arr[i] alone is an odd number so we don't have any subsequences with odd sum
+			dp_odd[i] = 0
+
+			# If we add arr[i] to a subsequence with even sum, the sum remains even
+			# If we add arr[i] to a subsequence with odd sum, the sum remains odd
+			dp_even[i] = (dp_even[i] + sum_even) % MOD
+			dp_odd[i] = (dp_odd[i] + sum_odd) % MOD
+		else:
+			dp_even[i] = 0 # arr[i] cannot be alone
+			dp_odd[i] = 1 # arr[i] alone
+
+			# If we add arr[i] to a subsequence with even sum, the sum becomes odd
+			# If we add arr[i] to a subsequence with odd sum, the sum becomes even
+			dp_even[i] = (dp_even[i] + sum_odd) % MOD
+			dp_odd[i] = (dp_odd[i] + sum_even) % MOD
+
+		# Recalculate the partial sums
+		sum_even = (sum_even + dp_even[i]) % MOD
+		sum_odd = (sum_odd + dp_odd[i]) % MOD
+
+	# Return the number of subsequences with even sum
+	return sum_even
