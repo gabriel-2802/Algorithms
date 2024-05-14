3dd473c332ac093d30cfd3ae79cc34d1c4143259
gabriel-2802 <gabrielvalentine738@gmail.com>
2024-05-14 09:03:17 +0300

    template

---
+++ applied_algorithms_mario/Template.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    void read() {
+
+    }
+
+    void solve() {
+
+    }
+
+    void print() {
+
+    }
+}
+
+public class Template {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.read();
+        solution.solve();
+        solution.print();
+    }
+}
+
+class Pair<T1, T2> {
+    T1 first;
+    T2 second;
+
+    Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Pair) {
+            Pair<?, ?> other = (Pair<?, ?>) obj;
+            return first.equals(other.first) && second.equals(other.second);
+        }
+        return false;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        return first.hashCode() ^ second.hashCode();
+    }
+    
+}
+
+class MyScanner {
+    private BufferedReader br;
+    private StringTokenizer st;
+
+    public MyScanner(Reader reader) {
+        br = new BufferedReader(reader);
+    }
+
+    public String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    public String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
---
+++ applied_algorithms_mario/template.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <limits>
+#include <unordered_set>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+#define INF numeric_limits<int>::max()
+
+class Solution {
+    public:
+        void read() {
+            // code here
+        }
+
+        void solve() {
+            // code here
+        }
+
+        void print() {
+            // code here
+        }
+};
+
+// add this to use pair as key in unordered_map and unordered_set
+struct pair_hash {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const {
+        auto h1 = hash<T1>{}(p.first);
+        auto h2 = hash<T2>{}(p.second);
+        return h1 ^ (h2 << 1);
+    }
+};
+
+int main() {
+    std::ios::sync_with_stdio(false);  
+    Solution solution;
+    solution.read();
+    solution.solve();
+    solution.print();
+    return 0;
+}
