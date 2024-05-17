aba062577e77c258e0f109b10f86225e61f22655
gabriel-2802 <gabrielvalentine738@gmail.com>
2024-05-17 11:52:24 +0300

    johnson tested

--- graphs/floyd_and_johnson.py
+++ graphs/floyd_and_johnson.py
@@ -2,7 +2,7 @@ from typing import *
 from numbers import Number
 import heapq
 import collections
-from graphs.minimum_paths_one_to_many import bellman_ford, dijkstra, MAX
+from dijkstra_and_bellman_ford import bellman_ford, dijkstra, MAX
 
 
 INF = 10 ** 20
@@ -59,7 +59,7 @@ def roy_floyd(n: int, w: List[List[int]]) -> Tuple[List[List[int]], List[List[in
 
 
 """
-	*NOT Tested*
+	*Tested*
 
 	--> Calculates the shortest path between all pairs of nodes in a graph
 	--> It is recommended to use this algorithm when the graph has few edges
@@ -67,8 +67,6 @@ def roy_floyd(n: int, w: List[List[int]]) -> Tuple[List[List[int]], List[List[in
 
 	@Complexity: O(V^2 log V + VE)
 	@params:
-		n: number of nodes
-		m: number of edges
 		adj: adj[i] will contain an array of pairs (node, weight) representing
 			the fact that there is an edge from i to node with cost weight
 	@return:
@@ -77,41 +75,48 @@ def roy_floyd(n: int, w: List[List[int]]) -> Tuple[List[List[int]], List[List[in
 			- dist contains the shortest distances between nodes
 			- parent contains the parent of each node in the shortest path
 """
-def johnson(n: int, m: int, adj: List[List[Tuple[int, int]]]) -> Tuple[bool, List[List[int]], List[List[int]]]:
-	d = [[0] * (n + 1) for _ in range(n + 1)]
-	p = [[0] * (n + 1) for _ in range(n + 1)]
-	edges = []
-	source_node = 0
+def johnson(adj: List[List[Tuple[int, int]]]) -> Tuple[bool, List[List[int]], List[List[int]]]:
+	n = len(adj) # 1 based indexing (node 0 is not used)
+	new_graph = adj + [[]]  # Adding a new node as the last node
 
-	for i in range(n):
-		edges.append((source_node, i, 0))
+	# Add a new dummy node and connect it to all other nodes with weight 0
+	for i in range(1, n + 1):
+		new_graph[-1].append((i, 0))
 
-	for i in range(n):
-		for neigh in adj[i]:
-			edges.append((i, neigh[0], neigh[1]))
-
-	# Bellman-Ford
-	(neg_cycle, [d_bellman_ford, p_bellman_ford]) = bellman_ford(edges, source_node)
-	if neg_cycle:
+	try:
+		# Bellman-Ford from the dummy node
+		[d_bellman_ford, _] = bellman_ford(new_graph, n)
+		
+		# Reweight the edges
+		reweighted_graph = [[] for _ in range(n + 1)]
+		for u in range(n + 1):
+			for v, weight in new_graph[u]:
+				new_weight = weight + d_bellman_ford[u] - d_bellman_ford[v]
+				reweighted_graph[u].append((v, new_weight))
+		
+		# Initialize distance and parent matrices
+		d = [[0 if i == j else MAX for j in range(n + 1)] for i in range(n + 1)]
+		p = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
+		
+		# Run Dijkstra for each node
+		for i in range(1, n + 1):
+			dijkstra_dist, dijkstra_parent = dijkstra(reweighted_graph, i)
+			for j in range(1, n + 1):
+				if dijkstra_dist[j] < MAX:
+					d[i][j] = dijkstra_dist[j] - d_bellman_ford[i] + d_bellman_ford[j]
+					p[i][j] = dijkstra_parent[j]
+		
+		# Remove the dummy node from the results (node n)
+		d.pop()
+		for i in range(n):
+			d[i].pop()
+		p.pop()
+		for i in range(n):
+			p[i].pop()
+		
+		return False, d, p
+	
+	except ValueError:
+		# Negative cycle detected
 		return True, [], []
 
-	for i in range(n):
-		for neigh in adj[i]:
-			neigh_weight = neigh[1] - d_bellman_ford[i] + d_bellman_ford[neigh[0]]
-			edges.append((i, neigh[0], neigh_weight))
-
-	# Dijkstra
-	for i in range(n):
-		[d_dijkstra, p_dijkstra] = dijkstra(adj, i)
-		for j in range(n):
-			if d_dijkstra[j] == MAX:
-				d[i][j] = 0
-				continue
-			d[i][j] = d_dijkstra[j]
-			p[i][j] = p_dijkstra[j]
-
-	for i in range(n):
-		for j in range(n):
-			d[i][j] += d_bellman_ford[j] - d_bellman_ford[i]
-
-	return False, d, p
--- graphs/push_relabel.py
+++ graphs/push_relabel.py
@@ -1,18 +1,8 @@
 from typing import *
 
-def push_relabel_alg(graph: List[List[int]], source: int, target: int) -> int:
-	n = len(graph) # 1 indexed
-   
-	def init():
-	  pass
-   
-	def push(u: int, v: int):
-		pass
-   
-	def relabel(u: int):
-	  pass
-   
-   return 1
+def push_relabel_alg(graph: List[List[int]], source: int, target: int):
+    pass
+	
 
 
 
