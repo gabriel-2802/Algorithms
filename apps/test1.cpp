da2384ea9a9343ff72f4166c0d5c2d4234f243af
mariosam23 <sampetrumario11@gmail.com>
2024-05-18 10:53:37 +0300

    1

--- apps/test1.cpp
+++ apps/test1.cpp
@@ -1,12 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
+int n, start;
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	
+	cin >> n;
+	cin >> start;
+	vector<vector<int>> jumps(n);
+
+	for (int i = 0; i < n - 1; i++) {
+		int k;
+		cin >> k;
+		for (int j = 0, x; j < k; j++) {
+			cin >> x;
+			jumps[i].push_back(x);
+		}
+	}
+
+	vector<int> dp(n, INT_MAX);
+	dp[start] = 0;
+
+	for (int i = start; i < n; i++) {
+		if (dp[i] != INT_MAX) {
+			for (int jump : jumps[i]) {
+				if (i + jump < n) dp[i + jump] = min(dp[i + jump], dp[i] + 1);
+			}
+		}
+	}
+
+	auto ans = dp[n - 1] == INT_MAX ? -1 : dp[n-1];
+	cout << ans;
+
 	return 0;
 }
