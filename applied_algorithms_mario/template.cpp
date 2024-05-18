57d749444f264c506d2d4195b7870c6e954df415
mariosam23 <sampetrumario11@gmail.com>
2024-05-18 08:08:07 +0300

    template

--- applied_algorithms_mario/template.cpp
+++ applied_algorithms_mario/template.cpp
@@ -1,34 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define INF numeric_limits<int>::max()
 
 typedef long long ll;
 typedef unsigned long long ull;
 
-class Solution {
- public:
-	int n;
-
-   	void read() {
-		
-	}
-
-	void solve() {
-		
-	}
-
-	void print() {
-		
-	}
-};
-
 int main() {
 	std::ios::sync_with_stdio(false);
-	Solution *s = new Solution();
-	s->read();
-	s->solve();
-	s->print();
-	delete s;
+	
 	return 0;
 }
---
+++ apps/test2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	
+	return 0;
+}
